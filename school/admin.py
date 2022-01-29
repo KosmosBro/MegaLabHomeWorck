37216f5ea55c6840e3c931660d9a05f38028c266
admin <kuligturan@mail.ri>
2022-01-29 20:40:14 +0600

    2-th task to do models

--- school/admin.py
+++ school/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from school.models import Position, Employees, EmployeesCharacteristic, EmployeesAddress, Salary, Projects, \
-    EmployeesInProjects
+    EmployeesInProjects, Company, Leader, Product, Sales, Address, PaymentAccount
 
 admin.site.register(Position)
 admin.site.register(Employees)
@@ -10,3 +10,9 @@ admin.site.register(EmployeesAddress)
 admin.site.register(Salary)
 admin.site.register(Projects)
 admin.site.register(EmployeesInProjects)
+admin.site.register(Company)
+admin.site.register(Leader)
+admin.site.register(Product)
+admin.site.register(Sales)
+admin.site.register(Address)
+admin.site.register(PaymentAccount)
--- school/models.py
+++ school/models.py
@@ -2,33 +2,33 @@ from django.db import models
 
 
 class Position(models.Model):
-    title = models.CharField(max_length=100, null=True)
+    title = models.CharField(max_length=100, )
 
     def __str__(self):
         return self.title
 
 
 class Employees(models.Model):
-    last_name = models.CharField(max_length=100, null=False)
-    first_name = models.CharField(max_length=100, null=False)
-    second_name = models.CharField(max_length=100, null=False)
-    position = models.ManyToManyField(Position, blank=False)
+    last_name = models.CharField(max_length=100, )
+    first_name = models.CharField(max_length=100, )
+    second_name = models.CharField(max_length=100, )
+    position = models.ManyToManyField(Position)
 
     def __str__(self):
         return self.first_name
 
 
 class EmployeesCharacteristic(models.Model):
-    title = models.CharField(max_length=200, null=False)
-    employees = models.ManyToManyField(Employees, null=False)
+    title = models.CharField(max_length=200)
+    employees = models.ManyToManyField(Employees)
 
     def __str__(self):
         return self.title
 
 
 class EmployeesAddress(models.Model):
-    address_title = models.CharField(max_length=50, null=False)
-    number_address = models.IntegerField(max_length=10, null=False)
+    address_title = models.CharField(max_length=50)
+    number_address = models.IntegerField(max_length=10)
     employees = models.OneToOneField(Employees, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -36,24 +36,76 @@ class EmployeesAddress(models.Model):
 
 
 class Salary(models.Model):
-    salary = models.IntegerField(max_length=100, null=False)
-    employees = models.ManyToManyField(Employees, null=False)
+    salary = models.IntegerField(max_length=100)
+    employees = models.ManyToManyField(Employees)
+
+    def __str__(self):
+        return self.salary
 
 
 class EmployeesInProjects(models.Model):
-    days_in_project = models.IntegerField(null=False)
-    employees = models.ManyToManyField(Employees, null=False)
+    days_in_project = models.IntegerField()
+    employees = models.ManyToManyField(Employees)
+
+    def __str__(self):
+        return self.days_in_project
 
 
 class Projects(models.Model):
-    title = models.CharField(max_length=100, null=False)
-    stat_date = models.DateField(null=False)
-    end_date = models.DateField(null=False)
+    title = models.CharField(max_length=100, )
+    stat_date = models.DateField()
+    end_date = models.DateField()
     in_project = models.OneToOneField(EmployeesInProjects, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 
+class Company(models.Model):
+    title = models.CharField(max_length=50, )
+
+    def __str__(self):
+        return self.title
 
 
+class Leader(models.Model):
+    last_name = models.CharField(max_length=20, )
+    second_name = models.CharField(max_length=20, )
+    name = models.CharField(max_length=20, )
+    company = models.OneToOneField(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.last_name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50, )
+    price = models.IntegerField()
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Sales(models.Model):
+    count = models.IntegerField()
+    date_of_sale = models.DateField()
+    product = models.ManyToManyField(Product, )
+    company = models.ManyToManyField(Company, )
+
+    def __str__(self):
+        return self.count
+
+
+class Address(models.Model):
+    title = models.CharField(max_length=50, )
+    number = models.IntegerField()
+    company = models.ManyToManyField(Company, )
+
+    def __str__(self):
+        return self.title
+
+
+class PaymentAccount(models.Model):
+    number = models.IntegerField()
+    company = models.ManyToManyField(Company, )
